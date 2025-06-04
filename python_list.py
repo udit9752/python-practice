78a32e6cc287ccf7de1c0d2ac2354e9477558085
udit tiwari <udittiwari903@gmail.com>
2025-06-05 01:11:29 +0530

    Python List

---
+++ python_list.py
@@ -0,0 +1,17 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+print(list[0])
+list.append(11)
+print(list)
+list.insert(0,45)
+list.remove(3)
+print(list)
+list.extend([12,13,14])
+print(list)
+list.pop(0)
+print(list)
+list[1]=46
+print(list)
+list[1:5]=[47,48,49,50]
+print(list)
+list.remove(50)
+print(list)
